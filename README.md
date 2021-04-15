966f2c0b6a10e28569d5ad5fa83c4b1603bb61f7
DingDean <me@dingkwz.com>
2021-04-15 18:31:50 +0800

    look ma, i can call kqueue now!

--- README.md
+++ README.md
@@ -7,8 +7,10 @@
 3. 为什么不是 epoll？**因为类 bsd 系统上只有 kqueue, epoll 是 linux 系才有的指令**
 4. 为什么在调用了 `kqueue` 后，要调用 `fcntl` 复制一个 `fd` 呢？**Explaine Me!!!! 官方的解释可以在 https://man7.org/linux/man-pages/man2/open.2.html 的 O_CLOEXEC 部分找到，但需要自己的理解。**
 5. 我如何监听想要关注的事件和资源？**(调用 kevent 方法)**
+6. 首先你得获取你想监听的资源的 `fd`，比如一个 `socket` 的 `fd`。
+7. 其次你得声明需要监听的事件，即 `kevent`
 
-## 所有由代码直接调用的系统指令
+## 所有由代码直接调用的系统指令和相关概念
 
-1. [kqueue](https://developer.apple.com/library/archive/documentation/System/Conceptual/ManPages_iPhoneOS/man2/kqueue.2.html) 用于获取系统的 `kqueue` 实例。
+1. [kqueue](https://www.freebsd.org/cgi/man.cgi?kqueue) 用于获取系统的 `kqueue` 实例。
 2. [fcntl](https://man7.org/linux/man-pages/man2/fcntl.2.html) 操作文件的指令集。
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use libc;
-
-type Filter = i16;
-type UData = libc::intptr_t;
+use std::io::prelude::*;
+use std::net::{TcpListener, TcpStream};
+use std::os::unix::io::AsRawFd;
 
 /// Helper macro to execute a system call that returns an `io::Result`.
 #[allow(unused_macros)]
@@ -16,25 +16,100 @@ macro_rules! syscall {
     }};
 }
 
-#[allow(unused_macros)]
-macro_rules! kevent {
-    ($id: expr, $filter: expr, $flags: expr, $data: expr) => {
-        libc::kevent {
-            ident: $id as libc::uintptr_t,
-            filter: $filter as Filter,
-            flags: $flags,
-            fflags: 0,
-            data: 0,
-            udata: $data as UData,
-        }
-    };
+fn check_flags(flags: u16) {
+    let fs = vec![
+        libc::EV_CLEAR,
+        libc::EV_RECEIPT,
+        libc::EV_ADD,
+        libc::EV_EOF,
+        libc::EV_POLL,
+        libc::EV_DISABLE,
+        libc::EV_DISPATCH,
+        libc::EV_ENABLE,
+        libc::EV_ERROR,
+    ];
+    fs.iter().for_each(|f| {
+        eprintln!("{}", *f & flags == *f);
+    })
 }
 
-fn main() {
+fn main() -> std::io::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:8888")?;
+    // 这是我们要监听的资源
+    // let mut stream = TcpStream::connect(listener.local_addr()?)?;
+
     // 获取 epoll 实例
-    syscall!(kqueue())
-        .and_then(|kq| syscall!(fcntl(kq, libc::F_SETFD, libc::FD_CLOEXEC)).map(|_| kq))
-        .map(|kq| {
-            // do something with kq
+    let kq = syscall!(kqueue())
+        .and_then(|kq| syscall!(fcntl(kq, libc::F_SETFD, libc::FD_CLOEXEC)).map(|_| kq))?;
+
+    let flag = libc::EV_CLEAR | libc::EV_RECEIPT | libc::EV_ADD;
+
+    let ev = libc::kevent {
+        ident: listener.as_raw_fd() as libc::uintptr_t,
+        filter: libc::EVFILT_READ,
+        flags: flag,
+        fflags: 0,
+        data: 0,
+        udata: 0 as *mut libc::c_void,
+    };
+
+    let mut changelist = vec![ev];
+
+    eprintln!("正在注册事件偏好，不会阻塞...");
+    syscall!(kevent(
+        kq,
+        changelist.as_ptr(),
+        1,
+        changelist.as_mut_ptr(),
+        1,
+        std::ptr::null(),
+    ))
+    .map(|n| {
+        eprintln!("成功注册 {} 个事件！", n);
+        changelist.iter().for_each(|e| {
+            if e.flags & libc::EV_ERROR == libc::EV_ERROR {
+                eprintln!("错误 {}", std::io::Error::from_raw_os_error(e.data as i32));
+            }
         });
+        ()
+    })
+    .or_else(|err| {
+        if err.raw_os_error() == Some(libc::EINTR) {
+            Ok(())
+        } else {
+            Err(err)
+        }
+    })?;
+
+    let mut events: Vec<libc::kevent> = Vec::with_capacity(10);
+
+    loop {
+        // t += 1;
+        eprintln!("正在检查是否有事件发生");
+        let n = syscall!(kevent(
+            kq,
+            std::ptr::null(),
+            0,
+            events.as_mut_ptr(),
+            events.capacity() as libc::c_int,
+            &libc::timespec {
+                tv_sec: 5,
+                tv_nsec: 5000
+            }
+        ))?;
+        if n > 0 {
+            eprintln!("正在处理 {} 个事件", n);
+            unsafe {
+                events.set_len(n as usize);
+            };
+            events.iter().for_each(|e| {
+                eprintln!("正在返回数据...");
+                let (mut stream, addr) = listener.accept().unwrap();
+                stream.write(b"HTTP/1.1 200 OK\r\n\r\n").unwrap();
+                stream.flush().unwrap();
+            })
+        }
+    }
+
+    // Ok(())
 }
